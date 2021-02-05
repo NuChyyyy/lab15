a34c9d4ae991f51e917c99ffed3e7409474fd056
NuChyyyy <76035494+NuChyyyy@users.noreply.github.com>
2021-02-05 16:35:25 +0700

    lab15-1*edit
    Signed-off-by: NuChyyyy <76035494+NuChyyyy@users.noreply.github.com>

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -32,3 +32,54 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *d, int N, int M)
+{
+	cout << fixed << setprecision(2);
+	for (int i = 0; i < N * M; i++)
+	{
+		cout << *(d + i);
+		if ((i + 1) % M == 0)
+			cout << endl;
+		else
+			cout << " ";
+	}
+}
+
+void randData(double *d, int N, int M)
+{
+	for (int i = 0; i < N * M; i++)
+	{
+		*(d + i) = (rand() % 101) / 100.00;
+	}
+}
+
+void findRowSum(const double *d, double *s, int n, int m)
+{
+	double sum = 0;
+	int i = 0;
+	for (int j = 0; j < n * m; j++)
+	{
+		sum += *(d + j);
+		if ((j + 1) % m == 0)
+		{
+			*(s + i) = sum;
+			sum = 0;
+			i++;
+		}
+	}
+}
+
+void findColSum(const double *d, double *s, int n, int m)
+{
+	double sum;
+	for(int i = 0; i < m; i++)
+	{
+		sum = *(d+i);
+		for(int j = m+i;j < n*m; j+=m)
+		{
+			sum += *(d+j);
+		}
+		*(s+i) = sum;
+	}
+}
